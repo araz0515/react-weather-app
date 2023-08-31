b4239458926a3d7d9bd07b6978f808fabbdd11fb
araz0515 <arazkubtanjian@gmail.com>
2023-08-31 16:16:20 -0400

    Added the GitHub repo url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,19 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather app</div>;
+  return (
+    <div className="App">
+      <h1> Weather app</h1>
+      <footer>
+        This project was coded by Araz Kubtanjian and is
+        <a
+          href="https://github.com/araz0515/react-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
