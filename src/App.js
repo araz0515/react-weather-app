abf01eda1af6da044beb09080eb5f243940bd328
araz0515 <arazkubtanjian@gmail.com>
2023-08-31 16:43:55 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,17 +3,19 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1> Weather app</h1>
-      <footer>
-        This project was coded by Araz Kubtanjian and is
-        <a
-          href="https://github.com/araz0515/react-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1> Weather app</h1>
+        <footer>
+          This project was coded by Araz Kubtanjian and is
+          <a
+            href="https://github.com/araz0515/react-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
