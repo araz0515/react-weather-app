457a622b488c14b840f55e305f0f1b4b5884426c
araz0515 <arazkubtanjian@gmail.com>
2023-09-01 01:36:35 -0400

    Added API weather data & if/else statement

--- src/Weather.js
+++ src/Weather.js
@@ -4,22 +4,20 @@ import axios from "axios";
 
 export default function Weather() {
   let [city, setCity] = useState(null);
-  let [loaded, setLoaded] = useState(false);
-  let [temperature, setTemperature] = useState(null);
+  let [weatherData, setWeatherData] = useState({ loaded: false });
 
   function displayTemperature(response) {
     console.log(response.data);
-    if (loaded) {
-      setTemperature(response.data.temperature.current);
-    }
-  }
 
-  function handleSearch(event) {
-    setLoaded(true);
-    event.preventDefault();
-    let apiKey = "ea34ad36f9tbf5ceb9037o7457b5a408";
-    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-    axios.get(apiUrl).then(displayTemperature);
+    setWeatherData({
+      loaded: true,
+      city: response.data.city,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      wind: response.data.wind.speed,
+      description: response.data.condition.description,
+      icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    });
   }
 
   function handleUpdate(event) {
@@ -27,57 +25,91 @@ export default function Weather() {
     setCity(event.target.value);
   }
 
-  return (
-    <div className="Weather">
-      <form onClick={handleSearch}>
-        <div className="row">
-          <div className="col-md-10">
-            <input
-              type="search"
-              placeholder="Search city..."
-              className="form-control mb-2"
-              onChange={handleUpdate}
-            />
-          </div>
-          <div className="col-md-2">
-            <input
-              type="submit"
-              value="Search"
-              className="btn btn-primary w-100"
-            />
-          </div>
-        </div>
-      </form>
+  function handleSearch(event) {
+    event.preventDefault();
+    let apiKey = "ea34ad36f9tbf5ceb9037o7457b5a408";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(displayTemperature);
+  }
 
-      <div className="row pt-4">
-        <div className="col-md-8 ">
+  if (weatherData.loaded) {
+    return (
+      <div className="Weather">
+        <form onClick={handleSearch}>
           <div className="row">
-            <div className="col-md-5 d-flex pe-0">
-              <img
-                src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png"
-                alt="weather icon"
-                className="me-2"
+            <div className="col-md-10">
+              <input
+                type="search"
+                placeholder="Search city..."
+                className="form-control mb-2"
+                onChange={handleUpdate}
               />
-              <h1 className="degree">{Math.round(temperature)}</h1>
-              <p>˚C | ˚F</p>
             </div>
-            <div className="col-md-6 weather-info">
-              <ul>
-                <li>Precipitation: 0%</li>
-                <li>Humidity: 48%</li>
-                <li>Wind: 13 km/h</li>
-              </ul>
+            <div className="col-md-2">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
             </div>
           </div>
-        </div>
-        <div className="col-md-4 end-column ">
-          <h2 className="m-0 city-name">{city}</h2>
-          <ul className="lh-sm date">
-            <li>Thursday 4:00 pm</li>
-            <li>Mostly cloudy</li>
-          </ul>
+        </form>
+
+        <div className="row pt-4">
+          <div className="col-md-8 ">
+            <div className="row">
+              <div className="col-md-5 d-flex pe-0">
+                <img
+                  src={weatherData.icon}
+                  alt="weather icon"
+                  className="me-2"
+                />
+                <h1 className="degree">
+                  {Math.round(weatherData.temperature)}
+                </h1>
+                <p>˚C | ˚F</p>
+              </div>
+              <div className="col-md-6 weather-info">
+                <ul>
+                  <li>Humidity: {weatherData.humidity}%</li>
+                  <li>Wind: {Math.round(weatherData.wind)} km/h</li>
+                </ul>
+              </div>
+            </div>
+          </div>
+          <div className="col-md-4 end-column ">
+            <h2 className="m-0 city-name">{weatherData.city}</h2>
+            <ul className="lh-sm date">
+              <li>Thursday 4:00 pm</li>
+              <li className="text-capitalize">{weatherData.description}</li>
+            </ul>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    return (
+      <div className="Weather">
+        <form onClick={handleSearch}>
+          <div className="row">
+            <div className="col-md-10">
+              <input
+                type="search"
+                placeholder="Search city..."
+                className="form-control mb-2"
+                onChange={handleUpdate}
+              />
+            </div>
+            <div className="col-md-2">
+              <input
+                type="submit"
+                value="Search"
+                className="btn btn-primary w-100"
+              />
+            </div>
+          </div>
+        </form>
+      </div>
+    );
+  }
 }
