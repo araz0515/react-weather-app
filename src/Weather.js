25463669a6f3bf6613943e1e13fb472184eec94d
araz0515 <arazkubtanjian@gmail.com>
2023-09-01 22:54:04 -0400

    removed unused component

--- src/Weather.js
+++ src/Weather.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
 import "./Weather.css";
 import axios from "axios";
-import FormattedDate from "./FormattedDate";
 import WeatherData from "./WeatherData";
 
 export default function Weather() {
