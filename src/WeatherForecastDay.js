888da8d08661cc016e5d813063230232276d9941
araz0515 <arazkubtanjian@gmail.com>
2023-09-03 03:22:19 -0400

    removed console.log

--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -10,7 +10,7 @@ export default function WeatherForecastDay(props) {
     let temperature = Math.round(props.data.temperature.minimum);
     return `${temperature}˚`;
   }
-  console.log(props.data.time);
+
   function day() {
     let date = new Date(props.data.time * 1000);
     let day = date.getDay();
